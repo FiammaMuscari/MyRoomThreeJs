3b32fdc92acd8aa8cbf36cbe6d0dd1f9df08861b
Fiamma Muscari <fiammamuscari@gmail.com>
2022-10-08 22:10:13 -0300

    [Update]:ThemeSwitch

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -1,14 +1,34 @@
 import * as THREE from 'three';
 import Experience from "../Experience.js";
 import GSAP from 'gsap'
+import GUI from 'lil-gui'; 
 export default class Environment {
   constructor() {
     this.experience = new Experience();
     this.scene= this.experience.scene;
     this.resources = this.experience.resources;
-
+    this.gui = new GUI();
+    this.obj = {
+      colorObj:{r:0,g:0, b:0},
+      intensity:3,
+    }
     this.setSunlight();
+    this.setGUI();
   }
+
+  setGUI(){
+    this.gui.addColor(this.obj,'colorObj').onChange(()=>{
+      this.sunLight.color.copy(this.obj.colorObj)
+      this.ambientLight.color.copy(this.obj.colorObj)
+      console.log(this.obj.colorObj)
+    })
+    this.gui.add(this.obj,'intensity', 0, 10).onChange(()=>{
+      this.sunLight.intensity = this.obj.intensity
+      this.sunLight.ambientLight = this.obj.intensity
+    })
+  }
+
+
   setSunlight() {
     this.sunLight = new THREE.DirectionalLight("#ffffff", 3);
     this.sunLight.castShadow = true;
@@ -28,14 +48,14 @@ this.ambientLight = new THREE.AmbientLight("#ffffff", 1);
 switchTheme(theme){
   if(theme==='dark'){
     GSAP.to(this.sunLight.color,{
-      r:0/255,
-      g:0/255,
-      b:0/255,
+      r: 0.20784313725490197,
+      g: 0.20784313725490197,
+      b: 0.9764705882352941,
     }),
     GSAP.to(this.ambientLight.color,{
-      r:0/255,
-      g:0/255,
-      b:0/255,
+      r: 0.20784313725490197,
+      g: 0.20784313725490197,
+      b: 0.9764705882352941,
     })
   }else{
     GSAP.to(this.sunLight.color,{

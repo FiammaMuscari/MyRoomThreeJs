e1e29871cc6bfb61f9f9c1829b8c4814a362e0a7
Fiamma Muscari <fiammamuscari@gmail.com>
2022-10-11 23:17:24 -0300

    [Update]: Responsive ScrollTrigger

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -9,12 +9,12 @@ export default class Sizes extends EventEmitter{
         this.aspect = this.width/this.height;
         this.pixelRation = Math.min(window.devicePixelRatio, 2)
 
-        window.addEventListener ("resize", () =>{
+        window.addEventListener("resize", () => {
             this.width = window.innerWidth;
             this.height = window.innerHeight;
-            this.aspect = this.width/this.height;
-            this.pixelRation = Math.min(window.devicePixelRatio, 2)
-            this.emit("resize"); 
+            this.aspect = this.width / this.height;
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+            this.emit("resize");
 
         })
     }
--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -4,41 +4,34 @@ import GSAP from "gsap"
 import {ScrollTrigger} from "gsap/ScrollTrigger.js"
 
 export default class Controls {
-  constructor() {
+    constructor() {
         this.experience = new Experience();
         this.scene = this.experience.scene;
-        this.resources = this.experience.resources;
         this.sizes = this.experience.sizes;
+        this.resources = this.experience.resources;
         this.time = this.experience.time;
         this.camera = this.experience.camera;
-        this.room = this.experience.world.room.actualRoom
+        this.room = this.experience.world.room.actualRoom;
+        
         GSAP.registerPlugin(ScrollTrigger);
+        this.setScrollTrigger();
+    }
+    setScrollTrigger() {
+        ScrollTrigger.matchMedia({
+            //Desktop
+            "(min-width: 969px)": () => {
+                console.log("fired desktop");
+                },
+            // Mobile
+            "(max-width: 968px)": () => {
+                console.log("fired mobile");
+            },
+            // all
+            all: () => {                
+            },
+        });
+    }
+    resize() {}
 
-        this.setPath();
-      }
-
-      setPath(){
-        console.log(this.room)
-        this.timeline = new GSAP.timeline()
-        this.timeline.to(this.room.position,{
-          x: () =>{
-            return this.sizes.width * 0.0012
-          },
-          scrollTrigger: {
-            trigger:".first-move",
-            markers: true,
-            start:"top top",
-            end:"bottom bottom",
-            scrub:0.6,
-            invalidateOnRefresh:true,
-          },
-        })
-      }
-  
-
-  resize() {}
-  update() {
-
-
-  }
+    update() {}
 }

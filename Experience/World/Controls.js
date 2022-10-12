43cb2e3962f214a7c717613d033239c1d80fa66b
Fiamma Muscari <fiammamuscari@gmail.com>
2022-10-12 00:33:02 -0300

    [Update]: Responsive ScrollTrigger

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -17,11 +17,52 @@ export default class Controls {
         this.setScrollTrigger();
     }
     setScrollTrigger() {
+      //different animations scroll for each screen size
         ScrollTrigger.matchMedia({
             //Desktop
             "(min-width: 969px)": () => {
                 console.log("fired desktop");
+              //First section -----------------------------------
+                this.firstMoveTimeline = new GSAP.timeline({
+                  scrollTrigger:{
+                    trigger: ".first-move",
+                    start:"top top",
+                    end:"bottom bottom",
+                    scrub: 0.6,
+                    invalidateOnRefresh:true,
+                  },
+                })
+                this.firstMoveTimeline.to(this.room.position, {
+                  x: ()=>{
+                    return this.sizes.width * 0.0014
+                  }
+                })
+
+              //Second section -----------------------------------
+              this.secondMoveTimeline = new GSAP.timeline({
+                scrollTrigger:{
+                  trigger: ".second-move",
+                  start:"top top",
+                  end:"bottom bottom",
+                  scrub: 0.6,
+                  invalidateOnRefresh:true,
                 },
+              })
+              this.secondMoveTimeline.to(this.room.position, {
+                x: ()=>{
+                  return 1
+                },
+                z: ()=>{
+                  return this.sizes.height * 0.0032
+                }
+              })
+              this.secondMoveTimeline.to(this.room.scale, {
+                x:0.4,
+                y:0.4,
+                z:0.4,
+              })
+
+              },
             // Mobile
             "(max-width: 968px)": () => {
                 console.log("fired mobile");
